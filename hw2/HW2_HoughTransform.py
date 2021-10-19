6cfb2212a88c9b78bdf361eb3a53eb4c0ae13102
lucetre <lucetre@naver.com>
2021-10-19 13:46:22 +0900

    Finish HW #2

--- hw2/HW2_HoughTransform.py
+++ hw2/HW2_HoughTransform.py
@@ -8,17 +8,16 @@ from matplotlib import pyplot as plt
 
 # parameters
 datadir = './data'
-resultdir='./results'
+resultdir = './results'
 eps = np.finfo(float).eps
 
 # you can calibrate these parameters
-sigma=2
-highThreshold=0.1
-lowThreshold=0.1
-rhoRes=2
-thetaRes=math.pi/180
-nLines=20
-
+sigma = 2
+highThreshold = 0.4
+lowThreshold = 0.2
+rhoRes = 2
+thetaRes = math.pi/180
+nLines = 20
 
 def ConvFilter(Igs, G):
     for s in G.shape:
@@ -65,12 +64,12 @@ def EdgeDetection(Igs, sigma, highThreshold, lowThreshold):
     G = GaussianFilter(sigma)
     image = ConvFilter(Igs, G)
     
-    sobel = [np.array([[-1,  0,  1],
-                       [-2,  0,  2],
-                       [-1,  0,  1]]),
-             np.array([[ 1,  2,  1],
-                       [ 0,  0,  0],
-                       [-1, -2, -1]])]
+    sobel = [np.array([[-1, 0, 1],
+                       [-2, 0, 2],
+                       [-1, 0, 1]]),
+             np.array([[ 1, 2, 1],
+                       [ 0, 0, 0],
+                       [-1,-2,-1]])]
     
     Ix = ConvFilter(image, sobel[0])
     Iy = ConvFilter(image, sobel[1])
@@ -119,38 +118,23 @@ def HoughLines(H, rhoRes, thetaRes, nLines):
     lTheta = thetaRes * (n_index %  H.shape[1])
     return lRho, lTheta
 
-def find_segment(overlap):
-    overlap_len = len(overlap)
-    flip_index = np.nonzero(np.concatenate(([overlap[0]], overlap[:-1] != overlap[1:], [True])))[0]
-    true_false_lengths = np.diff(flip_index)
-
-    # If there is no intersection, then return (0, 0)
-    if len(true_false_lengths) == 0:
-        return 0, 0
-
-    longest_length = true_false_lengths[0]
-    longest_end_index = flip_index[0] + true_false_lengths[0] - 1
-    last_length = true_false_lengths[0]
-    last_end_index = flip_index[0] + true_false_lengths[0] - 1
-
-    for i in range((len(true_false_lengths) - 1) // 2):
-        current_false_len = true_false_lengths[2 * i + 1]
-        current_true_len = true_false_lengths[2 * i + 2]
-        last_end_index = last_end_index + current_true_len + current_false_len
-
-        if last_length + current_true_len > 4 * current_false_len and current_false_len < 4 * current_true_len and 40 * current_false_len < overlap_len:
-            last_length = last_length + current_true_len + current_false_len
-        else:
-            last_length = current_true_len
-
-        if last_length > longest_length:
-            longest_length = last_length
-            longest_end_index = last_end_index
-
-    longest_start_index = longest_end_index - longest_length + 1
-    return longest_start_index, longest_end_index
-
 def HoughLineSegments(lRho, lTheta, Im):
+    def FindInterval(I):
+        interval = (0, 0)
+        index = np.nonzero(np.concatenate(([I[0]], I[:-1] != I[1:])))[0]
+        if (len(index) < 2): return interval
+
+        lengths = np.diff(index)
+        cur = (index[0], index[0]+lengths[0]-1)
+        for i in range(len(lengths) // 2):
+            if cur[1]-cur[0] > interval[1]-interval[0]:
+                interval = cur
+            try1, try2 = lengths[2*i+1], lengths[2*i+2]
+            start = cur[0] if cur[1]-cur[0] > 3*try1-try2 else cur[1]+try1+1
+            end = cur[1]+try1+try2
+            cur = (start, end)
+        return interval
+    
     h, w = Im.shape
     x = np.arange(w)
     y = np.arange(h)
@@ -171,7 +155,7 @@ def HoughLineSegments(lRho, lTheta, Im):
         x = np.clip(x, 0, w-1)
         y = np.clip(y, 0, h-1)
         
-        start, end = find_segment((Im[y, x] * mask) > 0)
+        start, end = FindInterval((Im[y, x] * mask) > 0)
         
         l_dict["start"] = (x[start], y[start])
         l_dict["end"]   = (x[end]  , y[end])
@@ -185,7 +169,7 @@ def PlotImage(img, name):
     plt.savefig(name, bbox_inches='tight', pad_inches=0)
     
 def PlotLines(img, name, lTheta, lRho):
-    plt.figure()
+    plt.figure(dpi=150)
     for theta, rho in zip(lTheta, lRho):
         if ((np.pi*0/4 <= theta < np.pi*1/4) or 
             (np.pi*3/4 <= theta < np.pi*5/4) or 
@@ -204,7 +188,7 @@ def PlotLines(img, name, lTheta, lRho):
     plt.savefig(name, bbox_inches='tight', pad_inches=0)    
 
 def PlotSegments(img, name, l):
-    plt.figure()
+    plt.figure(dpi=150)
     for l_dict in l:
         x = (l_dict["start"][0], l_dict["end"][0])
         y = (l_dict["start"][1], l_dict["end"][1])
@@ -217,7 +201,7 @@ def PlotSegments(img, name, l):
 def main():
     # read images
     for img_path in glob.glob(datadir + '/*.jpg'):
-        basename = os.path.basename(img_path)[:-4]
+        basename = os.path.basename(img_path).split('.')[0]
         
         # load grayscale image
         img = Image.open(img_path).convert("L")
@@ -226,16 +210,15 @@ def main():
 
         # Hough function
         Im, Io, Ix, Iy = EdgeDetection(Igs, sigma, highThreshold, lowThreshold)
+        PlotImage(Im, os.path.join(resultdir, basename+"_Im.png"))
+        
         H = HoughTransform(Im, rhoRes, thetaRes)
-        lRho,lTheta = HoughLines(H, rhoRes, thetaRes, nLines)
+        PlotImage(H, os.path.join(resultdir, basename+"_H.png"))
+        
+        lRho, lTheta = HoughLines(H, rhoRes, thetaRes, nLines)   
+        PlotLines(Igs, os.path.join(resultdir, basename+"_lines.png"), lTheta, lRho)
         l = HoughLineSegments(lRho, lTheta, Im)
-
-        # saves the outputs to files
-        # Im, H, Im + hough line , Im + hough line segments
-        PlotImage(Im, os.path.join(resultdir, basename+"_Im.jpg"))
-        PlotImage(H, os.path.join(resultdir, basename+"_H.jpg"))
-        PlotLines(Igs, os.path.join(resultdir, basename+"_hough_lines.jpg"), lTheta, lRho)
-        PlotSegments(Igs, os.path.join(resultdir, basename+"_hough_segments.jpg"), l)
+        PlotSegments(Igs, os.path.join(resultdir, basename+"_segments.png"), l)
 
 if __name__ == '__main__':
     main()
